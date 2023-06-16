2c55568bfa949433cb18d2e85ea7e0189603676e
Tim Welch <tim.j.welch@gmail.com>
2023-06-16 07:48:07 -0700

    drop sub-devcontainers

--- common/scripts/cloneOrPullAll.sh
+++
@@ -1,2 +0,0 @@
-if cd /workspaces/geoprocessing; then git pull; else git clone https://github.com/seasketch/geoprocessing; fi
-# Add additional workspaces here
---
+++ common/scripts/setup.sh
@@ -0,0 +1,1 @@
+if cd /workspaces/geoprocessing; then git pull; else git clone https://github.com/seasketch/geoprocessing; fi
